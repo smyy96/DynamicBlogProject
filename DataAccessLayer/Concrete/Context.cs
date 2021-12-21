f4abf01ce336d3bf96651331622422bcea29871f
smyy <smyy.cskn@hotmail.com>
2021-12-22 02:39:51 +0300

    Entity Layer Katmanını oluşturma

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Baglantı adresinin tanımlandığı class
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //veritabanını baglama
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-5HVC58C\\SQLEXPRESS;database=CoreBlogDb;integrated security=true;");
+        }
+    }
+}
